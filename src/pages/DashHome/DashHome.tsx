0425fdb575681b3904920641e12fc76f739cd037
olaiide <horlaide909@gmail.com>
2022-03-23 05:32:49 -1200

    single room

--- src/pages/DashHome/DashHome.tsx
+++ src/pages/DashHome/DashHome.tsx
@@ -1,38 +1,98 @@
 import React from "react";
 import Navbar from "../../components/NavBar/NavBar";
-import { Container, Header, Wrapper, RoomGrid } from "./DashHomeElements";
-import Room from "../../assets/room1.jpg"
+import {
+  Container,
+  Header,
+  Wrapper,
+  RoomGrid,
+  RoomWrapper,
+  ContentWrapper,
+  Image,
+} from "./DashHomeElements";
+import Room from "../../assets/room1.jpg";
 // interface Props {
 //   type : string;
 //   price : string;
 //   available : boolean;
 // }
 const DashHome = () => {
-  const Data =  [
+  const Data = [
     {
-      type : "Superior room",
-      price : "#40",
-      available : 'True',
-      image : Room,
-    }
-  ]
+      type: "Superior room",
+      price: "#40",
+      available: "True",
+      image: Room,
+    },
+    {
+      type: "Superior room",
+      price: "#40",
+      available: "True",
+      image: Room,
+    },
+    {
+      type: "Superior room",
+      price: "#40",
+      available: "True",
+      image: Room,
+    },
+    {
+      type: "Superior room",
+      price: "#40",
+      available: "True",
+      image: Room,
+    },
+    {
+      type: "Superior room",
+      price: "#40",
+      available: "True",
+      image: Room,
+    },
+    {
+      type: "Superior room",
+      price: "#40",
+      available: "True",
+      image: Room,
+    },
+    {
+      type: "Superior room",
+      price: "#40",
+      available: "True",
+      image: Room,
+    },
+    {
+      type: "Superior room",
+      price: "#40",
+      available: "True",
+      image: Room,
+    },
+    {
+      type: "Superior room",
+      price: "#40",
+      available: "True",
+      image: Room,
+    },
+  ];
   return (
     <Container>
       <Wrapper>
-      <Navbar />
-      <Header>
-        <div>
-          <h2>Find and book rooms.</h2>
-          <h3>Search through array of rooms we've prepared for you.</h3>
-        </div>
-      </Header>
+        <Navbar />
+        <Header>
+          <div>
+            <h2>Find and book rooms.</h2>
+            <h3>Search through array of rooms we've prepared for you.</h3>
+          </div>
+        </Header>
       </Wrapper>
       <RoomGrid>
         {Data.map((item) => (
-          <div>
-            <img src={item.image} alt="room" />
-            <div>{item.type}</div>
-          </div>
+          <RoomWrapper>
+            <Image>
+              <img src={item.image} alt='room' width='100%' />
+            </Image>
+            <ContentWrapper>
+              <div>{item.type}</div>
+            </ContentWrapper>
+          </RoomWrapper>
         ))}
       </RoomGrid>
     </Container>
--- src/pages/DashHome/DashHomeElements.js
+++ src/pages/DashHome/DashHomeElements.js
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 import Landing from "../../assets/Landing2.jpg";
 
-export const Container = styled.div` 
-  min-height : 100vh;
-
-`
+export const Container = styled.div`
+  min-height: 100vh;
+`;
 export const Wrapper = styled.div`
   background-image: url(${Landing});
   height: 400px;
@@ -12,22 +11,46 @@ export const Wrapper = styled.div`
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
-  overflow : hidden;
+  overflow: hidden;
 `;
 export const Header = styled.div`
   color: white;
   font-weight: 600;
   font-size: 2rem;
-  display : flex;
-  justify-content : center;
-  align-items : center;
-  height : 100%;
-  div{
-      text-align : center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  div {
+    text-align: center;
   }
 `;
 export const RoomGrid = styled.div`
-  
-`
+  display: grid;
+  justify-content: center;
+  grid-template-columns: repeat(3, 0.1fr);
+  grid-gap: 30px;
+  width: 80%;
+  margin: 0 auto;
+  margin-top: 1rem;
+  background-color: ;
+`;
 export const RoomWrapper = styled.div`
-`
+  width: 330px;
+  height: px;
+/* border-bottom : 1px solid black; */
+/* border-left : 1px solid black; 
+border-right : 1px solid black;  */
+`;
+export const Image = styled.div`
+  border: none !important;
+  height : 200px;
+`;
+export const ContentWrapper = styled.div`
+   border-bottom : 1px solid black;
+border-left : 1px solid black;
+border-right : 1px solid black; 
+  height: 50px;
+  padding: 0;
+  margin: 0;
+`;
